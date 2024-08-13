800d56597c6d1671bcc8b7b04ba0cda89c521c0e
Saafo <saffordh@163.com>
2024-08-13 11:54:35 +0800

    Remove pkg_config, using CPATH instead
    Change-Id: I504b6948a7b1db99026bccccd0f9a834e8b172e5

--- build.rs
+++ build.rs
@@ -1,20 +1,12 @@
 use std::{env::var, path::PathBuf};
 
 use bindgen::{Builder, CargoCallbacks};
-use pkg_config::probe_library;
 
 fn main() {
     let out_path =
         PathBuf::from(var("OUT_DIR").expect("failed to get OUT_DIR environment variable"));
-    let library = probe_library("opusfile").expect("failed to probe library");
     let bindings = Builder::default()
         .header("wrapper.h")
-        .clang_args(
-            library
-                .include_paths
-                .iter()
-                .map(|include_path| format!("-I{}", include_path.display())),
-        )
         .parse_callbacks(Box::new(CargoCallbacks))
         .blocklist_file(".*stdio.h")
         .generate()
