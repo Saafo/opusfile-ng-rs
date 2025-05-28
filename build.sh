1df6249be7c72833d9a5dec43e432c96b33e8e46
Saafo <saffordh@163.com>
2025-05-28 22:19:37 +0800

    🏗️ add build.sh
    Change-Id: I70121b67e67d776e3535d49d107ffdd208acba64

---
+++ build.sh
@@ -0,0 +1,4 @@
+CPATH=/Users/saafo/Workspace/rust-sdk/deps/oggopus/include/libopus/:/Users/saafo/Workspace/rust-sdk/deps/oggopus/include/libopus/opus:/Users/saafo/Workspace/rust-sdk/deps/oggopus/include/opusfile \
+# PKG_CONFIG_PATH=/Users/saafo/Documents/Coding/opus/opusfile-ng-rs/opusfile \
+PKG_CONFIG_SYSROOT_DIR=/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk \
+cargo build --target aarch64-apple-ios
